11a46ac10607527d890156c6cc02ff51ea745e3a
DARK DAYS <117774135+DARK-DAYS0D@users.noreply.github.com>
2022-11-09 22:27:01 +0300

    darkhacktool.sh
    Wortex tim...🥀👍

---
+++ DarkHackTool/Dark-DaysHackTool/SystemUpdate.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## SystemUpdate ##
+
+function ok {
+    echo "ok" &> /dev/null
+}
+
+if [[ -f chk ]] ; then
+    ok
+else
+        clear
+        echo " 🐱‍👤DARK DAYS & WORTEX 🐱‍👤 & 🛡HACK TOOL🛡|                          
+"
+        echo "Gerekli araçlar kuruluyor lütfen bekleyin.."
+        {
+			apt install git -y
+			apt install python3 -y 
+			apt install python3-pip -y 	
+            apt install python -y 
+            apt install nmap -y 
+            apt install torghost -y 
+            apt install tor -y 
+            apt install default-jdk -y 
+			apt install openjdk-9-jdk-headless -y 			
+            apt install php -y 			
+			python3 -m pip install setuptools			
+            pip install mechanize 
+            pip install php			
+            pip install tor			
+            pip install curl			
+            pip install tor			
+            pip install stem					
+            pip3 install requests			
+            pip3 install clint		
+            pip3 install coloramas			
+        }
+		touch chk
+        echo "Gerekli araçlar kuruldu.."
+fi
